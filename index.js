df04095f4894dfebbd023c149b68ca69efa05258
bcha92@hotmail.com <bcha92@hotmail.com>
2021-09-30 00:35:27 -0400

    --update endpoints

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 // Node Express Import
-import express from "express";
+import express, { json } from "express";
 import cors from "cors";
 
 //Middleware Import
 import mongokeys from "./server/MongoMiddleware.js";
 
 // Handlers Import
+import { getUserReservations, getReservation } from "./server/handlers/reservations.js";
+import { getProperties, getProperty } from "./server/handlers/properties.js";
+
 import { testMongo } from "./server/handlers/mongotest.js"; // TEST ONLY!!!
-import { getUserReservations } from "./server/handlers/reservations.js";
 
 // Local Port to host application || 4000 by default
 const port = process.env.PORT || 4000;
@@ -16,7 +18,14 @@ const app = express(); // Initialized Express App
 app.use(cors()); // Cross-Origin Resource Sharing
 
 // Endpoints
+// GET List of Reservation by User ID
 app.get("/reservations/:userId", mongokeys, getUserReservations);
+// GET a Reservation by Reservation ID
+app.get("/reservations/:userId/:reservationId", mongokeys, getReservation);
+// GET List of Properties by User ID
+app.get("/properties", mongokeys, getProperties);
+// GET a Property by Property ID
+app.get("/properties/:propertyId", mongokeys, getProperty);
 
 app.get("/test", mongokeys, testMongo);
 // Error Handling
--- server/handlers/properties.js
+++ server/handlers/properties.js
@@ -3,75 +3,73 @@ import dotenv from "dotenv";
 dotenv.config();
 const { MONGO_URI } = process.env;
 
-// GET Reservations made by user ID
-export const getUserReservations = async (req, res) => {
-    const { userId } = req.params; // User Identification (Auth0 sub)
+// GET Properties by user ID
+export const getProperties = async (req, res) => {
     // Deconstructed res.locals
-    const { options, database, reservations } = res.locals;
+    const { options, database, properties } = res.locals;
     const mongo = new MongoClient(MONGO_URI, options);
 
     try {
         // Connect Mongo, begin session
         await mongo.connect();
         const db = mongo.db(database);
-        const results = await db.collection(reservations).find({ userId }).toArray();
+        const results = await db.collection(properties).find().toArray();
 
         // Results if no reservations are found
         if (results.length === 0) {
             res.status(400).json({
                 status: 400,
-                message: `No reservations found for User ID ${userId}.`,
+                message: `No properties found.`,
             })
         }
         // Results if any reservations are found
         else {
             res.status(200).json({
                 status: 200,
-                message: `Reservations found for User ID ${userId}.`,
+                message: `Properties found.`,
                 data: results,
             })
         }
     }
 
     catch (err) { // Error Handler
-        console.log("getUserReservation Error:", err);
+        console.log("getProperties Error:", err);
     }
     mongo.close(); // Disconnect Mongo, end session
 };
 
-// GET a Reservation by Reservation ID
-export const getReservation = async (req, res) => {
-    // User ID (Auth0 sub) and Reservation ID
-    const { reservationId } = req.params;
+// GET a Property by Property ID
+export const getProperty = async (req, res) => {
+    const { propertyId } = req.params; // Property ID
     // Deconstructed res.locals
-    const { options, database, reservations } = res.locals;
+    const { options, database, properties } = res.locals;
     const mongo = new MongoClient(MONGO_URI, options);
 
     try {
         // Connect Mongo, begin session
         await mongo.connect();
         const db = mongo.db(database);
-        const result = await db.collection(reservations).findOne({ _id: reservationId });
+        const result = await db.collection(reservations).findOne({ _id: propertyId });
 
         // Result if no reservation is found
         if (result === null) {
             res.status(400).json({
                 status: 400,
-                message: `Reservation ID ${reservationId} not found.`,
+                message: `The property you are searching for by ID ${propertyId} is not found.`,
             })
         }
-        // Result if reservation is found
+        // Result in reservation is found
         else {
             res.status(200).json({
                 status: 200,
-                message: `Reservation ID ${reservationId} found`,
+                message: `Propery ID ${propertyId} has been found.`,
                 data: result,
             })
         }
     }
 
-    catch (err) { // Error Handling
-        console.log("getReservation Error:", err);
+    catch (err) {
+        console.log("getProperty Error:", err);
     }
     mongo.close(); // Disconnect Mongo, end session
 };
