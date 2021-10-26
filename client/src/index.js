21659401884ea09d2a660239d4e4fc1c09a5b7f9
bcha92@hotmail.com <bcha92@hotmail.com>
2021-10-26 12:13:22 -0400

    --hidden secrets

--- client/src/index.js
+++ client/src/index.js
@@ -1,21 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import dotenv from "dotenv";
+dotenv.config();
+
+const { REACT_APP_GOOGLE_KEY, AUTH_ZERO_DOM, AUTH_ZERO_CLIENT } = process.env;
 
 // Auth0Provider
 import { Auth0Provider } from "@auth0/auth0-react";
 
 // PORT number access from process.env or 4000 by default
 export const PORT = `http://localhost:${process.env.PORT || 4000}`
-// THIS KEY WILL NO LONGER BE AVAILABLE AS OF DECEMBER 2021
-const REACT_APP_GOOGLE_KEY = "AIzaSyBs2WcPdaJrAKuN2LkPAR1f2p8nj4L6m0g";
 
 // Main REACTDOM renderer via html file in /public
 ReactDOM.render(
   <React.StrictMode>
     <Auth0Provider
-      domain="dev-abh4ezl1.us.auth0.com"
-      clientId="vZztKT6TjPBIE7wrnrLMKvCXUWu7VByV"
+      domain={AUTH_ZERO_DOM}
+      clientId={AUTH_ZERO_CLIENT}
       redirectUri={window.location.origin}
     ><App GK={REACT_APP_GOOGLE_KEY} />
     </Auth0Provider>
